94822ef0ef6cb879ef724a367e69b6465692ae00
Вячеслав Нестеренко <Tyuiu.NesterenkoVV@outlook.com>
2025-10-27 16:34:25 +0500

    Task3

---
+++ Tyuiu.NesterenkoVV.Sprint3.Task3.V1.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.NesterenkoVV.Sprint3.Task3.V1.Lib;
+namespace Tyuiu.NesterenkoVV.Sprint3.Task3.V1.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            char a = 'a';
+            string z = "have a nice time";
+            var res = ds.GetCharCount(z, a);
+            Assert.AreEqual(2, res);
+        }
+    }
+}
---
+++ Tyuiu.NesterenkoVV.Sprint3.Task3.V1/Program.cs
@@ -0,0 +1,34 @@
+﻿using Tyuiu.NesterenkoVV.Sprint3.Task3.V1.Lib;
+namespace Tyuiu.NesterenkoVV.Sprint3.Task3.V1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #3 | Выполнил: Нестеренко В. В. | ИИПБ-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Оператор foreach                                                  *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #1                                                              *");
+            Console.WriteLine("* Выполнил: Нестеренко Вячеслав Владимирович | ИИПБ-25-1                  *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Используя цикл foreach подсчитать количество букв “а”                   *");
+            Console.WriteLine("*  в строке: have a nice time *                                           *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            string z = "have a nice time";
+            char c = 'a';
+            var res = ds.GetCharCount(z, c);
+            Console.WriteLine(res);
+            Console.ReadLine();
+        }
+    }
+}
