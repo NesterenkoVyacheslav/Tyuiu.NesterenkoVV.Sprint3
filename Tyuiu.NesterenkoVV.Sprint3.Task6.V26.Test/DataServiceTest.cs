70f2459bb8db286793b2efd866dd4f4deeaec6ea
Вячеслав Нестеренко <Tyuiu.NesterenkoVV@outlook.com>
2025-10-28 15:33:14 +0500

    Task6

---
+++ Tyuiu.NesterenkoVV.Sprint3.Task6.V26.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.NesterenkoVV.Sprint3.Task6.V26.Lib;
+namespace Tyuiu.NesterenkoVV.Sprint3.Task6.V26.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int x = 15;
+            int y = 22;
+            int res = ds.GetSumTheDivisors(x, y);
+            Assert.AreEqual(33, res);
+        }
+    }
+}
---
+++ Tyuiu.NesterenkoVV.Sprint3.Task6.V26/Program.cs
@@ -0,0 +1,34 @@
+﻿using Tyuiu.NesterenkoVV.Sprint3.Task6.V26.Lib;
+namespace Tyuiu.NesterenkoVV.Sprint3.Task6.V26
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #3 | Выполнил: Нестеренко В. В. | ИИПБ-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Обработка целочисленной информации                                *");
+            Console.WriteLine("* Задание #6                                                              *");
+            Console.WriteLine("* Вариант #26                                                             *");
+            Console.WriteLine("* Выполнил: Нестеренко Вячеслав Владимирович | ИИПБ-25-1                  *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Напишите программу, которая ищет среди целых чисел,                     *");
+            Console.WriteLine("* принадлежащих числовому отрезку [15, 22] количество делителей           *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            int x = 15;
+            int y = 22;
+            var res = ds.GetSumTheDivisors(x, y);
+            Console.WriteLine(res);
+            Console.ReadLine();
+        }
+    }
+}
